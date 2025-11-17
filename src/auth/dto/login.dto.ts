0ba70767d10370285f338858a896979180c27d9b
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-17 13:45:32 -0300

    Added ValidationPipe, class-validator, class-transformer for validating request body against dto model

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'E-mail inválido' })
+  @IsNotEmpty({ message: 'E-mail não pode estar vazio' })
   email: string;
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode estar vazia' })
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //whitelist: true will remove any non-decorated values received in request body
+  //forbidNonWhitelisted: true will return a 400 bad response if non-decorated values are received in request body
+  //transform: true will convert request body into the appropriate dto object instance
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: false,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
