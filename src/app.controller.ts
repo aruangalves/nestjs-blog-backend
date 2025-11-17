a141520c979b0b057bbabab4a01adc22ec465837
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-17 10:51:40 -0300

    Prepared file for testing REST API through REST Client VSCode extension

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//This controller assumes the root route '/' for requests unless otherwise specified
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
