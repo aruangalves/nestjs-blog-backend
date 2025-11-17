9e9a27fc61b8fbdbbf4aaaf005dc87c159e7330f
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-17 14:34:11 -0300

    Custom pipe example usage

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
+    console.log(typeof id, id);
     return '/user controller response for user ' + id;
   }
 }
