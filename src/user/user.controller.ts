7dc6357f16dc02d85fc8b2188e82d795e861c7ef
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-18 10:38:50 -0300

    Prepared requests for dealing with JWT access token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,8 +15,6 @@ export class UserController {
   findOne(@Param('id', CustomParseIntPipe) id: number) {
     console.log(process.env.TEST || 'Default value not found');
     console.log(this.configService.get('TEST', 'Standard value if not found'));
-    //For essential env variables, throws a server error if value is not found and no default value is provided
-    console.log(this.configService.getOrThrow('TEST'));
     console.log(typeof id, id);
     return '/user controller response for user ' + id;
   }
