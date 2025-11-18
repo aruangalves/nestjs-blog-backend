3fcafc3d203a1df94e6495f15a7965665938547c
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-18 13:35:17 -0300

    Added route for changing user password

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'Nome de usuário precisa ser uma string' })
@@ -9,5 +9,6 @@ export class CreateUserDto {
   email: string;
   @IsString({ message: 'Senha precisa ser uma string' })
   @IsNotEmpty({ message: 'Senha não pode estar vazia' })
+  @MinLength(6, { message: 'Senha deve conter um mínimo de seis caracteres' })
   password: string;
 }
---
+++ src/user/dto/update-password.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode estar vazia' })
+  currentPassword: string;
+  @IsString({ message: 'Nova senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Nova senha não pode estar vazia' })
+  @MinLength(6, {
+    message: 'Nova senha deve conter um mínimo de seis caracteres',
+  })
+  newPassword: string;
+}
