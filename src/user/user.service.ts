1c7c4fbab28f0d709ab546b4340717f1fdaa8dc2
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-18 13:52:08 -0300

    Added readSelf and deleteSelf routes for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -110,4 +110,10 @@ export class UserService {
 
     return this.save(user);
   }
+
+  async remove(id: string) {
+    const user = await this.findOneByOrFail({ id });
+    await this.userRepository.delete({ id });
+    return user;
+  }
 }
