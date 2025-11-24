d3ab6228a92f3e77cecef091be12088b4730e7b7
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-24 14:18:08 -0300

    Added CORS and Helmet safety measures

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    }),
+  );
+
+  const corsWhiteList = parseCorsWhitelist(process.env.CORS_WHITELIST ?? '');
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined, //Browser info to protect clients
+      callback: (...args: any[]) => void,
+    ) => {
+      //Requests without origin or from a known origin within corsWhiteList are accepted
+      if (!origin || corsWhiteList.includes(origin)) {
+        return callback(null, true);
+      }
+      //Requests with origin, mas not known by server are denied
+      return callback(new Error('Not allowed by CORS'), false);
+    },
+  });
 
   //whitelist: true will remove any non-decorated values received in request body
   //forbidNonWhitelisted: true will return a 400 bad response if non-decorated values are received in request body
@@ -16,6 +40,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 void bootstrap();
